233e49d4485f84f82cec6140d3f60d7c01b5b0a4
sankalpitenmedia <sankalp@itenmedia.in>
2026-01-07 16:27:43 +0530

    Update the Init File

--- wp-content/plugins/energdive-user-system/app/Bootstrap/Init.php
+++ wp-content/plugins/energdive-user-system/app/Bootstrap/Init.php
@@ -1,13 +1,16 @@
 <?php
 namespace Energ\Bootstrap;
 
+use Energ\Auth\RequestOtp;
+use Energ\Auth\VerifyOtp;
+
 defined('ABSPATH') || exit;
 
 class Init {
 
     public static function boot() {
 
-        // Load Auth handlers
+        // Load Auth classes
         require_once ENERGDIVE_USER_PLUGIN_PATH . 'app/Auth/RequestOtp.php';
         require_once ENERGDIVE_USER_PLUGIN_PATH . 'app/Auth/VerifyOtp.php';
         require_once ENERGDIVE_USER_PLUGIN_PATH . 'app/Auth/Jwt.php';
@@ -16,13 +19,13 @@ class Init {
 
             register_rest_route('energdive/v1', '/auth/request-otp', [
                 'methods'  => 'POST',
-                'callback' => ['Energ\\Auth\\RequestOtp', 'handle'],
+                'callback' => [RequestOtp::class, 'handle'],
                 'permission_callback' => '__return_true',
             ]);
 
             register_rest_route('energdive/v1', '/auth/verify-otp', [
                 'methods'  => 'POST',
-                'callback' => ['Energ\\Auth\\VerifyOtp', 'handle'],
+                'callback' => [VerifyOtp::class, 'handle'],
                 'permission_callback' => '__return_true',
             ]);
 
