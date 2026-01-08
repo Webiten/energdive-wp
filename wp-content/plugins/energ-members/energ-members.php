be635ec17cd9c3aed5607fd62d9285fe11f701cf
sankalpitenmedia <sankalp@itenmedia.in>
2026-01-08 13:19:40 +0530

    commit energ-members

--- wp-content/plugins/energ-members/energ-members.php
+++ wp-content/plugins/energ-members/energ-members.php
@@ -1,7 +1,25 @@
 <?php
+// TEMP DEBUG (remove later)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+// Simple PSR-4 style autoloader for Energ\
+spl_autoload_register(function ($class) {
+    $prefix = 'Energ\\';
+    $base_dir = __DIR__ . '/app/';
+
+    if (strpos($class, $prefix) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, strlen($prefix));
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
 /*
 Plugin Name: Energ Members (Stable)
 Description: Member signup + OTP login + simple profile table (secure rebuild)
