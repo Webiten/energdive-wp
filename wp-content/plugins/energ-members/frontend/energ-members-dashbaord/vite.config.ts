3c8893ff7da01e054db480cc891e5e1deccbe569
sankalpitenmedia <sankalp@itenmedia.in>
2026-01-13 14:20:03 +0530

    Do commit

--- wp-content/plugins/energ-members/frontend/energ-members-dashbaord/vite.config.ts
+++ wp-content/plugins/energ-members/frontend/energ-members-dashbaord/vite.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     proxy: {
       "/wp-json": {
         target: "https://stage.energdive.com",
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
     },
   },
