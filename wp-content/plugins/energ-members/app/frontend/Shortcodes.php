ca574bd19773baf8eeb19e317cd9fc57f40dbd45
sankalpitenmedia <sankalp@itenmedia.in>
2026-01-21 14:17:16 +0530

    Commit All Changes

--- wp-content/plugins/energ-members/app/frontend/Shortcodes.php
+++ wp-content/plugins/energ-members/app/frontend/Shortcodes.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Energ\Frontend;
 
 class Shortcodes
@@ -6,14 +7,16 @@ class Shortcodes
     public static function register()
     {
         add_shortcode('energ_members_dashboard', [self::class, 'dashboard']);
+        add_shortcode('energ_members_auth', [self::class, 'auth']);
     }
 
     public static function dashboard()
     {
-        if (!is_user_logged_in()) {
-            return '<p>Please login to access dashboard.</p>';
-        }
-
         return '<div id="energ-dashboard-root"></div>';
+    }
+
+    public static function auth()
+    {
+        return '<div id="energ-auth-root"></div>';
     }
 }
