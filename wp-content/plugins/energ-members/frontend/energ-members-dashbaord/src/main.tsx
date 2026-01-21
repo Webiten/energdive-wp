584a7e44e0cd9e1587552fcd4b3aa83b3fcbe0f3
sankalpitenmedia <sankalp@itenmedia.in>
2026-01-21 15:27:44 +0530

    Commit Main.tsx

--- wp-content/plugins/energ-members/frontend/energ-members-dashbaord/src/main.tsx
+++ wp-content/plugins/energ-members/frontend/energ-members-dashbaord/src/main.tsx
@@ -2,8 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
 
-const el = document.getElementById("energ-dashboard-root");
+const container = document.getElementById("energ-dashboard-root");
 
-if (el) {
-  createRoot(el).render(<App />);
+if (container) {
+  createRoot(container).render(<App />);
 }
