a4fd504e2cbf432f90286c4d9b22d2ec2bf1fe65
sankalpitenmedia <sankalp@itenmedia.in>
2026-01-21 18:04:41 +0530

    Commit Chnages

--- wp-content/plugins/energ-members/frontend/energ-members-dashbaord/src/app/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-export function saveTokens(access: string, refresh: string) {
-  localStorage.setItem("access_token", access);
-  localStorage.setItem("refresh_token", refresh);
-}
-
-export function clearTokens() {
-  localStorage.removeItem("access_token");
-  localStorage.removeItem("refresh_token");
-}
